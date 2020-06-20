8e9aa910d24513c07ff54591481692ae266e3723
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2020-06-20 10:14:53 -0400

    pull out page models

---
+++ Integral.Web/Extensions/StringExtension.cs
@@ -0,0 +1,12 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.WebUtilities;
+
+namespace Integral.Functions
+{
+    public static class StringExtension
+    {
+        public static string Encode(this string value) => WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(value));
+
+        public static string Decode(this string value) => Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(value));
+    }
+}
