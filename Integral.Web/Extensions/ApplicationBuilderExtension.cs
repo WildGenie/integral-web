e87288af8b3819c82802c7cb5fb4bad89d209f5c
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2020-08-18 14:16:40 -0400

    complete web socket extension

--- Integral.Web/Extensions/ApplicationBuilderExtension.cs
+++ Integral.Web/Extensions/ApplicationBuilderExtension.cs
@@ -1,18 +1,19 @@
-﻿using System.Net.WebSockets;
+﻿using System;
+using System.Net.WebSockets;
 using Microsoft.AspNetCore.Builder;
 
 namespace Integral.Extensions
 {
     public static class ApplicationBuilderExtension
     {
-        public static void AddWebSockets(this IApplicationBuilder applicationBuilder, WebSocketOptions webSocketOptions)
+        public static void UseWebSockets(this IApplicationBuilder applicationBuilder, Action<WebSocket> accept, WebSocketOptions webSocketOptions)
         {
             applicationBuilder.UseWebSockets(webSocketOptions);
             applicationBuilder.Use(async (context, next) =>
             {
                 if (context.WebSockets.IsWebSocketRequest)
                 {
-                    WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
+                    accept(await context.WebSockets.AcceptWebSocketAsync());
                 }
                 else
                 {
